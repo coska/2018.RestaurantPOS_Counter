20f42ee7553e50e19332d337bec723c83c036e39
edwinyoon <ep160101@gmail.com>
2018-08-13 09:30:14 -0400

    Add Action types

--- store/home/actionTypes.js
+++ store/home/actionTypes.js
@@ -1,1 +1,5 @@
-export const SET_NAME = 'SET_NAME';
+export const SET_NAME = 'SET_NAME'
+export const ADD_STAFF = 'ADD_STAFF'
+export const GET_ALL_STAFFS = 'GET_ALL_STAFFS'
+export const UPDATE_STAFF = 'UPDATE_STAFF'
+export const DELETE_STAFF = 'DELETE_STAFF'
--- store/home/actions.js
+++ store/home/actions.js
@@ -1,7 +1,21 @@
-import * as types from './actionTypes';
+import * as types from './actionTypes'
 
-export const setName = (name) => {
-  return dispatch => {
-    dispatch({ type: types.SET_NAME, name });
-  };
-};
+export const setName = name => (dispatch) => {
+  dispatch({ type: types.SET_NAME, name })
+}
+export const addStaff = staffInfo => (dispatch) => {
+  // Add API call
+  dispatch({ type: types.ADD_STAFF, staffInfo /* return with allStaffs */ })
+}
+export const getAllStaffs = () => (dispatch) => {
+  // Add API call
+  dispatch({ type: types.GET_ALL_STAFFS /* return data from api call */ })
+}
+export const updateStaff = staffInfo => (dispatch) => {
+  // Add API call
+  dispatch({ type: types.UPDATE_STAFF, staffInfo })
+}
+export const deleteStaff = staffName => (dispatch) => {
+  // Add API call
+  dispatch({ type: types.DELETE_STAFF, staffName })
+}
